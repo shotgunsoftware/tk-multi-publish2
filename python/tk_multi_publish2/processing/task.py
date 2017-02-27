c7b831bd3447c2f15fbacfeab36a98505026a5eb
Manne Öhrström <manne@shotgunsoftware.com>
2017-02-27 15:59:17 +0000

    docstrings

--- python/tk_multi_publish2/processing/task.py
+++ python/tk_multi_publish2/processing/task.py
@@ -14,60 +14,88 @@ logger = sgtk.platform.get_logger(__name__)
 
 class Task(object):
     """
-    A plugin instance or the particular action that
-    should be carried out by a plugin on an item.
+    A task is a particular unit of work which can to be carried
+    by the publishing process. A task can be thought of as a
+    'plugin instance', e.g a particular publishing plugin operating
+    on a particular collector item.
     """
 
     def __init__(self, plugin, item):
+        """
+        :param plugin: The plugin instance associated with this task
+        :param item: The collector item associated with this task
+        """
         self._plugin = plugin
         self._item = item
         self._settings = []
         self._enabled = False
         self._required = False
 
-
     def __repr__(self):
         return "<Task: %s for %s >" % (self._plugin, self._item)
 
     def set_plugin_defaults(self, required, enabled):
+        """
+        Set the
+        :param required:
+        :param enabled:
+        """
         self._required = required
         self._enabled = enabled
 
     @property
     def item(self):
+        """
+        The item associated with this Task
+        """
         return self._item
 
     @property
     def plugin(self):
+        """
+        The plugin associated with this Task
+        """
         return self._plugin
 
     @property
-    def settings(self):
-        return self._settings
-
-    @property
     def required(self):
+        """
+        Returns if this Task is required by the publishing
+        """
         return self._required
 
     @property
     def enabled(self):
+        """
+        Returns if this
+        @return:
+        """
         return self._enabled
 
     @property
     def settings(self):
-
-        # TODO - writable per task settings pleeeease!
+        """
+        Dictionary of settings associated with this Task
+        """
+        # TODO - make settings configurable per task
         return self.plugin.settings
 
     def validate(self):
+        """
+        Validate this Task
 
+        :returns: True if validation succeeded, False otherwise.
+        """
         return self.plugin.run_validate(self.settings, self.item)
 
-
     def publish(self):
-
+        """
+        Publish this Task
+        """
         self.plugin.run_publish(self.settings, self.item)
 
     def finalize(self):
-
+        """
+        Finalize this Task
+        """
         self.plugin.run_finalize(self.settings, self.item)
