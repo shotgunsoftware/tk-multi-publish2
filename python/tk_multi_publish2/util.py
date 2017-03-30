bc3bf84433c6a60b357c446c141228b5b3bfd5cf
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2017-03-30 15:14:52 -0400

    Updates to support discussed zero config stand alone workflows
    * mostly updates to the utils methods to support file/folder publishing
    * utilities for scanning folders for image sequences
    * get previous publishes for a given context, name, and path
    * method to clear statuses for previous publishes matchign a supplied sg publish dict

--- python/tk_multi_publish2/util.py
+++ python/tk_multi_publish2/util.py
@@ -13,6 +13,7 @@ import os
 import re
 
 import sgtk
+from sgtk.util.filesystem import copy_file, copy_folder
 
 # ---- globals
 
@@ -41,7 +42,70 @@ _COMMON_EXTENSIONS = {
 _MIMETYPES_LOOKUP_POPULATED = False
 
 
-# ---- util functions
+# ---- file/path util functions
+
+def copy_path_to_version(path, version, padding=3):
+    """
+    Makes a copy of the supplied path in the same directory, augmented with the
+    supplied version.
+
+    If the path is a folder, its contents will be copied into the new folder.
+
+    Returns the copied path.
+
+    Example 1::
+
+        new_file = copy_path_to_version("/path/to/my_file.v001.ext", 2)
+        print new_file
+        # /path/to/my_file.v002.ext
+
+    Example 2::
+
+        new_folder = copy_pathto_version("/path/to/folder.v0001", 2, padding=4)
+        print new_folder
+        # /path/to/folder.v0002
+
+    If the destination path already exists, no copy will occur and None will be
+    returned.
+
+    :param path: Path to copy.
+    :param version: The version to copy the path to.
+    :param padding: The number of digits to pad the version number.
+
+    :return: The newly created path.
+    """
+
+    logger = sgtk.platform.current_bundle().logger
+    logger.debug("Copying path '%s' to version %s..." % (path, version))
+
+    # break down the supplied path to its components in order to easily build
+    # the new version path
+    path_info = get_file_path_components(path)
+
+    # build the new path from the supplied path components plus the new version
+    padded_version = str(version).zfill(padding)
+    new_name = "%s.v%s" % (path_info["prefix"], padded_version)
+    if path_info["extension"]:
+        new_name = "%s.%s" % (new_name, path_info["extension"])
+
+    # construct the full path in the same folder
+    new_path = os.path.join(path_info["folder"], new_name)
+    logger.debug("New path: %s" % (new_path,))
+
+    if os.path.exists(new_path):
+        logger.warn("Path already exists. Not copying.")
+        return None
+
+    # now copy the source to the new path and return it
+    if os.path.isdir(path):
+        # folder
+        copy_folder(path, new_path)
+    else:
+        # file
+        copy_file(path, new_path)
+
+    return new_path
+
 
 def get_file_type_info(extension):
     """
@@ -95,126 +159,197 @@ def get_file_path_components(path):
 
     Returns file path components in the form::
 
-        # example 1: path="/path/to/the/file/my_file.v001.ext"
+        # example 1: file with version number
+        # path="/path/to/the/file/my_file.v001.ext"
 
         {
             "path": "/path/to/the/file/my_file.v001.ext",
             "folder": "/path/to/the/file" ,
+            "folder_name": "file",
             "filename": "my_file.v001.ext",
             "filename_no_ext": "my_file.v001",
             "extension": "ext",
             "prefix": "my_file",
             "version": 1,
-            "version_str": v001
+            "version_str": v001,
+            "version_padding": 3
         }
 
-        # example 1: path="/path/to/the/file/my_file.ext"
+        # example 2: file without version number
+        # path="/path/to/the/file/my_file.ext"
 
         {
             "path": "/path/to/the/file/my_file.ext",
             "folder": "/path/to/the/file" ,
+            "folder_name": "file",
             "filename": "my_file.ext",
             "filename_no_ext": "my_file",
             "extension": "ext",
             "prefix": "my_file",
             "version": None,
-            "version_str": None
+            "version_str": None,
+            "version_padding": None
+        }
+
+        # example 3: folder with version number
+        # path="/path/to/the/folder.v001"
+
+        {
+            "path": "/path/to/the/folder.v001",
+            "folder": "/path/to/the" ,
+            "folder_name": "the",
+            "filename": "folder.v001",
+            "filename_no_ext": "folder.v001",
+            "extension": None,
+            "prefix": "folder",
+            "version": 1,
+            "version_str": v001,
+            "version_padding": 3
+        }
+
+        # example 4: folder without version number
+        # path="/path/to/the/folder"
+
+        {
+            "path": "/path/to/the/folder",
+            "folder": "/path/to/the" ,
+            "folder_name": "the",
+            "filename": "folder",
+            "filename_no_ext": "folder",
+            "extension": None,
+            "prefix": "folder",
+            "version": None,
+            "version_str": None,
+            "version_padding": None
         }
 
     """
 
+    # ensure no trailing path separator
+    path = path.rstrip(os.sep)
+
     logger = sgtk.platform.current_bundle().logger
     logger.debug("Getting file path components for path: '%s'..." % (path,))
 
-    # the easy bits
+    # break it up into the major components
     (folder, filename) = os.path.split(path)
+
+    # extract the extension and remove the "."
     (filename_no_ext, extension) = os.path.splitext(filename)
+    if extension:
+        extension = extension.lstrip(".")
+    else:
+        # prevent extension = ""
+        extension = None
 
-    # remove the "." from the extension
-    extension = extension.lstrip(".")
+    # the folder name is the name of the parent directory
+    folder_name = os.path.split(folder)[-1]
 
-    # some default values in case we can't determine a version
-    prefix = filename_no_ext
-    version_str = None
-    version = None
+    # see if there is a version in the filename. must match .v### somewhere in
+    # the filename.
+    version_pattern = re.compile("(.*)\.(v(\d+))", re.IGNORECASE)
+    version_pattern_match = re.search(version_pattern, filename)
+    if version_pattern_match:
+        prefix = version_pattern_match.group(1)
+        version_str = version_pattern_match.group(2)
+        version = int(version_pattern_match.group(3))
+        version_padding = len(version_str.lstrip("v"))
+    else:
+        # no version detected. set these to default state
+        prefix = filename_no_ext
+        version_str = None
+        version = None
+        version_padding = None
 
-    # now figure out if there's a version stashed in the name. this looks for a
-    # pattern like "my_file.v###.ext" where #### is a version number. There may
-    # be additional common version patterns that we need to add to this logic as
-    # well.
-    version_match = re.search("(.*)\.(v([0-9]+))\.[^.]+$", filename)
-
-    if version_match:
-        prefix = version_match.group(1)
-        version_str = version_match.group(2)
-        version = int(version_match.group(3))  # remove leading zeros
+    if extension == version_str:
+        # probably a folder of the form: folder.v001
+        filename_no_ext = filename
+        extension = None
 
     file_info = dict(
         path=path,
         folder=folder,
+        folder_name=folder_name,
         filename=filename,
         filename_no_ext=filename_no_ext,
         extension=extension,
         prefix=prefix,
         version=version,
         version_str=version_str,
+        version_padding=version_padding
     )
 
     logger.debug("Extracted file components: %s" % (file_info,))
     return file_info
 
 
-def get_next_version_folder(folder, pattern="^v(\d{3})$"):
+def get_image_sequence_paths(folder):
     """
-    Returns the next available version subfolder within the supplied folder.
+    Given a folder, inspect the contained files to find what appear to be images
+    with frame numbers.
 
-    :param str folder: The path path to a folder containing subfolders
-        representing versions.
-    :param str pattern: A regex pattern matching the version subfolders. The
-        default value matches the letter "v" followed by a 3 digit padded
-        version number. The pattern should match the entire subfolder name
-        and should include one set of parenthesis to allow matching of the
-        version number.
-
-    :returns: An integer representing the next sequential version folder
-
-    :raises ValueError: if the supplied folder does not exist.
-
-    For example, if the supplied folder looks like this::
-
-        /path/to/some/folder/publish/
-            v001/
-            v002/
-            v003/
-
-    The return value would be an integer, 4. If there are no subfolders or no
-    folders matching the pattern, an integer value of 1 will be returned.
+    :param folder:
+    :return:
     """
 
     logger = sgtk.platform.current_bundle().logger
-    logger.debug("Getting the next version subfolder for: %s" % (folder,))
+    logger.debug("Looking for image sequences in folder: '%s'..." % (folder,))
 
-    if not os.path.exists(folder):
-        raise ValueError("The supplied folder does not exist: %s" % (folder,))
+    seq_paths = set()
 
-    # keep a running list of matched version numbers. at the end we'll return
-    # the highest version + 1. So if we start with 0 and find nothing, we'll end
-    # up with 1.
-    existing_versions = [0]
+    # list of filenames without the frame number to avoid unnecessary processing
+    processed_names = []
 
-    for subfolder_name in os.listdir(folder):
+    # examine the files in the folder
+    for file in os.listdir(folder):
 
-        # make sure we're only processing subfolders
-        if not os.path.isdir(os.path.join(folder, subfolder_name)):
+        file_path = os.path.join(folder, file)
+        if os.path.isdir(file_path):
+            # ignore this folder
             continue
 
-        version_match = re.match(pattern, subfolder_name)
-        if version_match:
-            # append the matched version number as an int to the list
-            existing_versions.append(int(version_match.group(1)))
+        file_info = get_file_path_components(file_path)
+        filename = file_info["filename"]
 
-    # return the next available version
-    return max(existing_versions) + 1
+        type_info = get_file_type_info(file_info["extension"])
+
+        # if the extension isn't an image type, no need to continue. the display
+        # name in the type info will be something specific like "Rendered Image"
+        # or "Image File". Should probably rethink this. feels fragile.
+        if not "image" in type_info[0].lower():
+            continue
+
+        # looking for any number of digits between dots: .0001. or .0999.
+        frame_pattern = re.compile("(\.(\d+)\.)")
+        frame_pattern_match = re.search(frame_pattern, filename)
+
+        if not frame_pattern_match:
+            # no frame number detected. carry on.
+            continue
+
+        # build up a fram padding string to replace the frame number
+        match_str = frame_pattern_match.group(1)
+        frame_str = frame_pattern_match.group(2)
+
+        # see if we've already processed this name (without the frame number)
+        filename_no_frame = filename.replace(match_str, ".")
+        if filename_no_frame in processed_names:
+            continue
+
+        # add this name to the list to avoid doing the steps below for a
+        # different frame number
+        processed_names.append(filename_no_frame)
+
+        padding = len(frame_str)
+        frame_format = ".%%0%dd." % (padding,)
+
+        seq_filename = file_info["filename"].replace(match_str, frame_format)
+        seq_path = os.path.join(file_info["folder"], seq_filename)
+
+        logger.debug("Found image sequence: %s" % (seq_path,))
+        seq_paths.add(seq_path)
+
+    return list(seq_paths)
 
 
 def is_audio(extension):
@@ -236,6 +371,133 @@ def is_video(extension):
     Returns True if the extension is a known video file type.
     """
     return _is_category(extension, "video")
+
+
+# ---- publish util functions
+
+def get_publishes(context, path, name):
+    """
+    Returns a list of SG published file dicts for any existing publishes that
+    match the supplied context, path, and name.
+
+    :param context: The context to search publishes for
+    :param path: The path to match against previous publishes
+    :param name: The name of the publish.
+
+    :return: A list of ``dict``s representing existing publishes that match
+        the supplied arguments.
+    """
+
+    publisher = sgtk.platform.current_bundle()
+
+    logger = publisher.logger
+    logger.debug(
+        "Getting other publishes for context: %s, path: %s, name: %s" %
+        (context, path, name)
+    )
+
+    # ask core to do a dry_run of a publish with the supplied criteria. this is
+    # a workaround for our inability to filter publishes by path. so for now,
+    # get a dictionary of data that would be used to create a matching publish
+    # and use that to get publishes via a call to find(). Then we'll filter
+    # those by their path field. Once we have the ability in SG to filter by
+    # path, we can replace this whole method with a simple call to find().
+    publish_data = sgtk.util.register_publish(
+        publisher.sgtk,
+        context,
+        path,
+        name,
+        version_number=None,
+        dry_run=True
+    )
+    logger.debug("Publish dry run data: %s" % (publish_data,))
+
+    # now build up the filters to match against
+    publish_filters = []
+    for field in ["code", "entity", "name", "project", "task"]:
+        publish_filters.append([field, "is", publish_data[field]])
+    logger.debug("Build publish filters: %s" % (publish_filters,))
+
+    # run the
+    publishes = publisher.shotgun.find(
+        "PublishedFile",
+        publish_filters,
+        ["path"]
+    )
+    logger.debug("Find returned %s matches: %s" % (len(publishes), publishes))
+
+    # next, extract the publish path from each of the returned publishes and
+    # compare it against the supplied path. if the paths match, we add the
+    # publish to the list of publishes to return.
+    logger.debug("Comparing publish paths...")
+    matching_publishes = []
+    for publish in publishes:
+        publish_path = sgtk.util.resolve_publish_path(publisher.sgtk, publish)
+        if publish_path and publish_path == path:
+            logger.debug("Path match: %s" % (path,))
+            matching_publishes.append(publish)
+        else:
+            logger.debug(
+                "Path mismatch: %s (input path) ! = %s (publish)" %
+                (path, publish_path)
+            )
+
+    return matching_publishes
+
+
+def clear_status_for_other_publishes(context, publish_data):
+    """
+    Clear the status of any other publishes matching the supplied publish data.
+
+    The loader app respects the status of publishes to determine which are
+    available for the user to load in their DCC. Because it is possible to
+    create a version entry in SG with the same path multiple times, this method
+    provides an easy way to clear the status of previous publishes for a given
+    path.
+
+    The publish data supplied should be the fully populated publish data
+    returned by a call to ``sgtk.util.register_publish()``.
+
+    :param publish_data: Dictionary of the current publish data (i.e. the
+        publish entry whose status will not be cleared).
+    """
+
+    publisher = sgtk.platform.current_bundle()
+
+    logger = publisher.logger
+    logger.debug("Clearing the status of any other publishes.")
+
+    # determine the path from the publish data. this will match the path that
+    # was used to register the publish
+    path = sgtk.util.resolve_publish_path(publisher.sgtk, publish_data)
+    name = publish_data["name"]
+
+    # get a list of all publishes matching this criteria
+    publishes = get_publishes(context, path, name)
+
+    if not publishes:
+        # no other publishes. nothing to do.
+        logger.debug("No other publishes detected for path: %s" % (path,))
+        return
+
+    # do a batch update of the other publishes to clear their status
+    batch_data = []
+    for publish in publishes:
+
+        # make sure we don't update the supplied publish
+        if publish["id"] == publish_data["id"]:
+            continue
+
+        # add the update info to the batch data list
+        batch_data.append({
+            "request_type": "update",
+            "entity_type": publish["type"],
+            "entity_id": publish["id"],
+            "data": {"sg_status_list": None}  # will clear the status
+        })
+
+    # execute all the updates!
+    publisher.shotgun.batch(batch_data)
 
 
 # ---- helper functions
