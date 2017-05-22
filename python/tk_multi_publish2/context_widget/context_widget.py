e1b093aee60e309965d27210a62a2e99d8575cbf
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2017-05-21 21:15:56 -0400

    WIP

--- python/tk_multi_publish2/context_widget/context_widget.py
+++ python/tk_multi_publish2/context_widget/context_widget.py
@@ -20,6 +20,9 @@ shotgun_menus = sgtk.platform.import_framework(
 shotgun_globals = sgtk.platform.import_framework(
     "tk-framework-shotgunutils", "shotgun_globals")
 
+settings = sgtk.platform.import_framework(
+    "tk-framework-shotgunutils", "settings")
+
 from .ui.context_editor_widget import Ui_ContextWidget
 
 logger = sgtk.platform.get_logger(__name__)
@@ -27,8 +30,9 @@ logger = sgtk.platform.get_logger(__name__)
 
 class ContextWidget(QtGui.QWidget):
     """
-    Widget which represents the current context in the form
-    of combo boxes which lets the user change the context.
+    Widget which represents the current context and allows the user to search
+    for a different context via search completer. A menu is also provided for
+    recent contexts as well as tasks assigned to the user.
 
     Emits a context_changed signal when the user selects a new context
     """
@@ -38,12 +42,20 @@ class ContextWidget(QtGui.QWidget):
 
     def __init__(self, parent):
         """
+        Initialize the widget
+
         :param parent: The model parent.
         :type parent: :class:`~PySide.QtGui.QObject`
         """
         super(ContextWidget, self).__init__(parent)
 
         self._bundle = sgtk.platform.current_bundle()
+
+        # get an instance of user settings to save/restore values across
+        # sessions
+        self._settings = settings.UserSettings(self._bundle)
+        self._settings_recent_contexts_key = "%s_recent_contexts" % (
+            self._bundle,)
 
         # we will do a bg query that requires an id to catch results
         self._schema_query_id = None
@@ -57,21 +69,50 @@ class ContextWidget(QtGui.QWidget):
         # menu for recent and user contexts
         self._context_menu = shotgun_menus.ShotgunMenu(self)
         self._context_menu.setObjectName("context_menu")
+        self._context_menu.addAction("Loading...")
 
         # keep a handle on all actions created
-        self._menu_actions = []
+        self._menu_actions = {
+            "My Tasks": [],
+            "Recent": []
+        }
 
         # set up the UI
         self.ui = Ui_ContextWidget()
         self.ui.setupUi(self)
 
-        # hide the menu button until the menu is built
-        self.ui.context_menu_btn.hide()
+    def closeEvent(self, event):
+        """
+        Prior to close, save the recents list of contexts to disk via user
+        settings.
+        """
+
+        # build a list of serialized recent contexts
+        serialized_contexts = []
+        for recent_action in self._menu_actions["Recent"]:
+            recent_context = recent_action.data()
+            serialized_contexts.append(recent_context.serialize())
+
+        # store the recent contexts on disk. the scope is per-project
+        settings.store(
+            self._settings_recent_contexts_key,
+            serialized_contexts,
+            scope=settings.UserSettings.SCOPE_PROJECT
+        )
 
     def set_up(self, task_manager):
+        """
+        Handles initial set up of the widget. Includes setting up menu, running
+        any background set up tasks, etc.
+
+        :param task_manager:
+        :return:
+        """
 
         # attach the context menu
         self.ui.context_menu_btn.setMenu(self._context_menu)
+        self._context_menu.aboutToShow.connect(
+            self._on_about_to_show_contexts_menu)
 
         # setup the search toggle
         self.ui.context_search_btn.toggled.connect(self._on_search_toggled)
@@ -104,8 +145,11 @@ class ContextWidget(QtGui.QWidget):
         self._schema_query_id = task_manager.add_task(
             self._query_publishedfile_entity_schema)
 
-        # now query all my assigned tasks
+        # query all my assigned tasks in a bg task
         self._my_tasks_query_id = task_manager.add_task(self._query_my_tasks)
+
+        # get recent contexts from user settings
+        self._get_recent_contexts()
 
     def _query_publishedfile_entity_schema(self):
 
@@ -142,6 +186,20 @@ class ContextWidget(QtGui.QWidget):
             order=order
         )
 
+    def _get_recent_contexts(self):
+
+        serialized_recent_contexts = self._settings.retrieve(
+            self._settings_recent_contexts_key,
+            default=[],
+            scope=settings.UserSettings.SCOPE_PROJECT
+        )
+
+        # turn these into QActions to add to the list of recents in the menu
+        for serialized_context in serialized_recent_contexts:
+            context = serialized_context.deserialize()
+            recent_action = self._get_qaction_for_context(context)
+            self._menu_actions["Recent"].append(recent_action)
+
     def _on_search_toggled(self, checked):
 
         if checked:
@@ -168,53 +226,43 @@ class ContextWidget(QtGui.QWidget):
         """
 
         if task_id == self._schema_query_id:
-            print "GOT SCHEMA RESULTS!"
-
-            published_file_entity_schema = result
-
-            # drill down into the schema to retrieve the valid types for the
-            # field. this is ugly, but will ensure we get a list no matter what
-            entity_types = published_file_entity_schema. \
-                get("entity", {}). \
-                get("properties", {}). \
-                get("valid_types", {}). \
-                get("value", [])
-
-            # always include Project and Tasks
-            entity_types.extend(["Project", "Task"])
-
-            logger.debug(
-                "Limiting context link completer to these entities: %s" %
-                (entity_types,)
-            )
-
-            # construct a dictionary that the search widget expects for
-            # filtering. This is a dictionary with the entity types as keys and
-            # values a list of search filters. We don't have any filters, so we
-            # just use empty list.
-            entity_types_dict = dict((k, []) for k in entity_types)
-
-            logger.debug(
-                "Setting searchable entity types to: %s" % (entity_types_dict,))
-
-            # now update the types for the completer
-            self.ui.context_search.set_searchable_entity_types(
-                entity_types_dict)
+            self._restrict_searchable_entity_types(result)
 
         elif task_id == self._my_tasks_query_id:
-            print "BUILDING TASKS MENU..."
-            self._build_tasks_menu(result)
+            self._build_my_tasks_actions(result)
 
-            # TODO:
-            # pull serialized recents from QSettings
-            # create the menu actions/groups
-            # as context changes, build QAction for the context, add to list
-            # only keep 5 most recent, unique contexts
-            # when menu is about to show, build with stored QActions
-            # on context change, save recents via QSettings
+    def _restrict_searchable_entity_types(self, published_file_entity_schema):
 
+        # drill down into the schema to retrieve the valid types for the
+        # field. this is ugly, but will ensure we get a list no matter what
+        entity_types = published_file_entity_schema. \
+            get("entity", {}). \
+            get("properties", {}). \
+            get("valid_types", {}). \
+            get("value", [])
 
-    def _build_tasks_menu(self, my_tasks):
+        # always include Project and Tasks
+        entity_types.extend(["Project", "Task"])
+
+        logger.debug(
+            "Limiting context link completer to these entities: %s" %
+            (entity_types,)
+        )
+
+        # construct a dictionary that the search widget expects for
+        # filtering. This is a dictionary with the entity types as keys and
+        # values a list of search filters. We don't have any filters, so we
+        # just use empty list.
+        entity_types_dict = dict((k, []) for k in entity_types)
+
+        logger.debug(
+            "Setting searchable entity types to: %s" % (entity_types_dict,))
+
+        # now update the types for the completer
+        self.ui.context_search.set_searchable_entity_types(
+            entity_types_dict)
+
+    def _build_my_tasks_actions(self, my_tasks):
 
         publisher = sgtk.platform.current_bundle()
 
@@ -227,37 +275,43 @@ class ContextWidget(QtGui.QWidget):
             for task in my_tasks:
                 task_context = publisher.sgtk.context_from_entity(
                     task["type"], task["id"])
-
-                context_display = _get_context_display(
-                    task_context, plain_text=True)
-                icon_path = _get_context_icon_path(task_context)
-
-                task_action = QtGui.QAction(self)
-                task_action.setText(context_display)
-                task_action.setIcon(QtGui.QIcon(icon_path))
-                task_action.triggered.connect(
-                    lambda tc=task_context: self._on_context_activated(tc))
+                task_action = self._get_qaction_for_context(task_context)
 
                 task_actions.append(task_action)
-                self._menu_actions.append(task_action)
-
-            if task_actions:
-                more_my_tasks_menu = None
-                if len(task_actions) > 5:
-                    more_my_tasks_menu = shotgun_menus.ShotgunMenu(self)
-                    more_my_tasks_menu.setTitle("More")
-                    more_my_tasks_menu.add_group(task_actions[5:], "My Tasks")
-
-                top_my_tasks_actions = task_actions[:5]
-                if more_my_tasks_menu:
-                    top_my_tasks_actions.append(more_my_tasks_menu)
-
-                self._context_menu.add_group(top_my_tasks_actions, "My Tasks")
-                self.ui.context_menu_btn.show()
-
+                self._menu_actions["My Tasks"].append(task_action)
         else:
             logger.info("No tasks found for current user: %s" % (
                 publisher.context.user,))
+
+    def _on_about_to_show_contexts_menu(self):
+
+        # clear and rebuild the menu since the recents section is dynamic
+        self._context_menu.clear()
+
+        # ---- build the "Recent" menu
+
+        recent_actions = self._menu_actions["Recent"]
+
+        if recent_actions:
+            self._context_menu.add_group(recent_actions, "Recent")
+
+        # ---- build the "My Tasks" menu
+
+        my_tasks_actions = self._menu_actions["My Tasks"]
+
+        if my_tasks_actions:
+
+            more_my_tasks_menu = None
+            if len(my_tasks_actions) > 5:
+                more_my_tasks_menu = shotgun_menus.ShotgunMenu(self)
+                more_my_tasks_menu.setTitle("More")
+                more_my_tasks_menu.add_group(my_tasks_actions[5:], "My Tasks")
+
+            top_my_tasks_actions = my_tasks_actions[:5]
+            if more_my_tasks_menu:
+                top_my_tasks_actions.append(more_my_tasks_menu)
+
+            self._context_menu.add_group(top_my_tasks_actions, "My Tasks")
 
     def _on_task_failed(self, task_id, group, message, traceback_str):
         """
@@ -274,6 +328,47 @@ class ContextWidget(QtGui.QWidget):
             "Error Message: %s.\n%s" % (message, traceback_str)
         )
 
+    def _add_to_recents(self, context):
+
+        recent_actions = self._menu_actions["Recent"]
+
+        matching_indexes = []
+        for i, recent_action in enumerate(recent_actions):
+            recent_context = recent_action.data()
+            if recent_context == context:
+                # contexts support __eq__ so this should be enough for comparing
+                matching_indexes.append(i)
+
+        if matching_indexes:
+            # context exists in recent list in one or more places. remove the
+            # QAction(s) and put one of them at the front of the list
+            recent_action = None
+            for match_index in matching_indexes:
+                recent_action = recent_actions.pop(match_index)
+        else:
+            # the context does not exist in the recents. add it
+            recent_action = self._get_qaction_for_context(context)
+
+        if recent_action:
+            recent_actions.insert(0, recent_action)
+
+        # only keep the 5 most recent
+        self._menu_actions["Recent"] = recent_actions[:5]
+
+    def _get_qaction_for_context(self, context):
+
+        context_display = _get_context_display(context, plain_text=True)
+        icon_path = _get_context_icon_path(context)
+
+        action = QtGui.QAction(self)
+        action.setText(context_display)
+        action.setIcon(QtGui.QIcon(icon_path))
+        action.setData(context)
+        action.triggered.connect(
+            lambda c=context: self._on_context_activated(c))
+
+        return action
+
     def set_context(self, context):
         """
         Register a context with the widget
@@ -282,6 +377,9 @@ class ContextWidget(QtGui.QWidget):
 
         self._context = context
         self._show_context(context)
+
+        # ensure the new context is added to the list of recents
+        self._add_to_recents(context)
 
     def _show_context(self, context):
 
@@ -303,8 +401,6 @@ class ContextWidget(QtGui.QWidget):
         self.ui.context_label.setText(hyperlink)
         self.ui.context_search_btn.setChecked(False)
         self.ui.context_search_btn.setDown(False)
-
-        # TODO: update the tooltip... this is the entity that the selected item...
 
     def _on_entity_activated(self, entity_type, entity_id, entity_name):
 
