2c9576a64b83c5f63a0b0dfa4d6a1dea90d03458
fhawi <firas.hawi@autodesk.com>
2017-08-30 19:05:09 -0400

    Added checkbox to context + related controls

--- python/tk_multi_publish2/publish_tree_widget/custom_widget_context.py
+++ python/tk_multi_publish2/publish_tree_widget/custom_widget_context.py
@@ -32,6 +32,7 @@ class CustomTreeWidgetContext(CustomTreeWidgetBase):
         # set up the UI
         self.ui = Ui_ContextWidget()
         self.ui.setupUi(self)
+        self.ui.checkbox.clicked.connect(self._on_checkbox_clicked_manually)
 
     @property
     def icon(self):
@@ -56,5 +57,22 @@ class CustomTreeWidgetContext(CustomTreeWidgetBase):
         """
         pass
 
-    def set_checkbox_value(self, state):
-        pass
+    def _on_checkbox_clicked_manually(self):
+        """
+        Callback that fires when the user clicks the checkbox
+        """
+
+        #the stateChanged signal is emitted before clicked() signal, which means
+        #that the value of checkState() below is the one after the user clicks the checkbox
+        state = self.ui.checkbox.checkState()
+
+        #When user clicks a context checkbox, we want it to choose between selecting all children 
+        #and unselecting all children, so 2 states.
+        #when user clicks on the checkbox of a child of the context, we have the possibility that some children are 
+        #selected, and others are not. In this case we need the QtCore.Qt.PartiallyChecked state. So 3 states... but this case is
+        #covered by underlying classes
+        if state != QtCore.Qt.PartiallyChecked:
+           self._tree_node.set_check_state(state)
+        else:
+           self.ui.checkbox.animateClick()
+  
