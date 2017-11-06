e34bb2a40009e51d6355add81b8a86e9e917a5f3
Luis Gomez Larez <luis.gomez.larez@autodesk.com>
2017-11-06 11:44:51 -0800

    44077. Code review refinements for feedback from Josh

--- hooks/submit_for_review.py
+++ hooks/submit_for_review.py
@@ -17,82 +17,65 @@ HookBaseClass = sgtk.get_hook_baseclass()
 
 class BasicSubmitForReviewPlugin(HookBaseClass):
     """
-    TODO: UPDATE TO SUBMIT FOR REVIEW!!!!!!
-
     Plugin for submitting a review in Shotgun.
 
-    This plugin is typically configured to act upon files that are dragged and
-    dropped into the publisher UI. It can also be used as a base class for
-    other file-based publish plugins as it contains standard operations for
-    validating and registering publishes with Shotgun.
+    This plugin is typically configured to act as a base class for
+    other DCC specific submit for review plugins as it contains standard operations for
+    validating and registering review submissions with Shotgun.
 
-    Once attached to a publish item, the plugin will key off of properties that
+    Once attached to an item, the plugin will key off of properties that
     are set on the item. These properties can be set via the collector or
     by subclasses prior to calling methods on this class.
 
     The only property that is required for the plugin to operate is the ``path``
     property. All of the properties understood by the plugin are documented
-    below::
+    below:
 
         Path properties
         -------------
 
-        path - The path to the file to be published.
-
-        sequence_paths - If set, implies the "path" property represents a
-            sequence of files (typically using a frame identifier such as %04d).
-            This property should be a list of files on disk matching the "path".
-            If a work template is provided, and corresponds to the listed
-            frames, fields will be extracted and applied to the publish template
-            (if set) and copied to that publish location.
+        path - The path to the sequence that will be submitted for review.
 
         Template properties
         -------------------
 
-        work_template - If set in the item properties dictionary, is used
+        work_template - Must be set in the item properties dictionary, is used
             to validate "path" and extract fields for further processing and
-            contextual discovery. For example, if configured and a version key
-            can be extracted, it will be used as the publish version to be
-            registered in Shotgun.
+            contextual discovery. Should be set  from the Nuke write node
+            and must be available. If not set submission will fail.
 
-        published_template - If set in the item properties dictionary, used to
-            determine where "path" should be copied prior to publishing. If
-            not specified, "path" will be published in place.
+        publish_template - Must be set in the item properties dictionary, used to
+            determine where the review movie should be copied prior to submission.
+            Should be set from the subclassed collector. If not specified the
+            submission will fail.
 
-        Publish properties
-        ------------------
+        Submission properties
+        ---------------------
 
-        publish_type - If set in the item properties dictionary, will be
-            supplied to SG as the publish type when registering "path" as a new
-            publish. If not set, will be determined via the plugin's "File Type"
-            setting.
-
-        publish_path - If set in the item properties dictionary, will be
-            supplied to SG as the publish path when registering the new publish.
-            If not set, will be determined by the "published_file" property if
-            available, falling back to publishing "path" in place.
-
-        publish_name - If set in the item properties dictionary, will be
-            supplied to SG as the publish name when registering the new publish.
-            If not available, will be determined by the "work_template"
-            property if available, falling back to the ``path_info`` hook
-            logic.
-
-        publish_version - If set in the item properties dictionary, will be
-            supplied to SG as the publish version when registering the new
-            publish. If not available, will be determined by the
-            "work_template" property if available, falling back to the
-            ``path_info`` hook logic.
-
-        publish_dependencies - A list of files to include as dependencies when
-            registering the publish. If the item's parent has been published,
-            it's path will be appended to this list.
-
-    This plugin will also set the properties on the item which may be useful for
-    child items.
+        publish_name - If set in the item properties dictionary, will be used
+            for output logging.
 
         sg_publish_data - The dictionary of publish information returned from
-            the tk-core register_publish method.
+            the tk-core register_publish method. This must be set by the publish_file
+            plugin that runs before this one. If not set, the review submission
+            will fail. 
+
+        color_space - Must be set in the item properties dictionary. It is
+            not set by the basic collector so it must be set by the 
+            subclassed one. If not available the item will not be accepted.
+            It is used for color correcting the output movie.
+
+        first_frame - Must be set in the item properties dictionary, It is
+            not set by the basic collector so it must be set by the
+            subclassed one. If not available, the item will not be accpeted.
+            Used to designate the first frame number of the sequence the
+            movie review will be created from.
+
+        last_frame - Must be set in the item properties dictionary, It is
+            not set by the basic collector so it must be set by the
+            subclassed one. If not available, the item will not be accpeted.
+            Used to designate the last frame number of the sequence the
+            movie review will be created from.
 
     """
 
@@ -126,16 +109,14 @@ class BasicSubmitForReviewPlugin(HookBaseClass):
         loader_url = "https://support.shotgunsoftware.com/hc/en-us/articles/219033078"
 
         return """
-        TODO: UPDATE TO SUBMIT FOR REVIEW!!!!!!
 
-        Publishes the file to Shotgun. A <b>Publish</b> entry will be
-        created in Shotgun which will include a reference to the file's current
-        path on disk. Other users will be able to access the published file via
-        the <b><a href='%s'>Loader</a></b> so long as they have access to
-        the file's location on disk.
+        Submits a movie file to Shotgun for review. An entry will be
+        created in Shotgun which will include a reference to the movie file's current
+        path on disk. Other users will be able to access the file via
+        the review app on the Shotgun website.
 
         <h3>File versioning</h3>
-        The <code>version</code> field of the resulting <b>Publish</b> in
+        The <code>version</code> field of the resulting review in
         Shotgun will also reflect the version number identified in the filename.
         The basic worklfow recognizes the following version formats by default:
 
@@ -147,10 +128,6 @@ class BasicSubmitForReviewPlugin(HookBaseClass):
 
         <br><br><i>NOTE: any amount of version number padding is supported.</i>
 
-        <h3>Overwriting an existing publish</h3>
-        A file can be published multiple times however only the most recent
-        publish will be available to other users. Warnings will be provided
-        during validation if there are previous publishes.
         """ % (loader_url,)
 
     @property
@@ -172,20 +149,7 @@ class BasicSubmitForReviewPlugin(HookBaseClass):
         The type string should be one of the data types that toolkit accepts
         as part of its environment configuration.
         """
-        return {
-            "File Types": {
-                "type": "list",
-                "default": [
-                    ["Movie", "mov", "mp4"],
-                ],
-                "description": (
-                    "List of file types to include. Each entry in the list "
-                    "is a list in which the first entry is the Shotgun "
-                    "published file type and subsequent entries are file "
-                    "extensions that should be associated."
-                )
-            },
-        }
+        return {}
 
     @property
     def item_filters(self):
@@ -204,7 +168,7 @@ class BasicSubmitForReviewPlugin(HookBaseClass):
         interest to this plugin. Only items matching the filters defined via the
         item_filters property will be presented to this method.
 
-        A publish task will be generated for each item accepted here. Returns a
+        A submit for review task will be generated for each item accepted here. Returns a
         dictionary with the following booleans:
 
             - accepted: Indicates if the plugin is interested in this value at
@@ -221,23 +185,64 @@ class BasicSubmitForReviewPlugin(HookBaseClass):
             instances.
         :param item: Item to process
 
-        :returns: dictionary with boolean keys accepted, required and enabled
+        :returns: dictionary with boolean keys accepted, required and enabled. 
+            This plugin makes use of the tk-multi-reviewsubmission app; if this
+            app is not available then the item will not be accepted, this method
+            will return a dictionary with the accepted field that has a value of
+            False.
+
+            Several properties on the item must also be present for it to be
+            accepted. The properties are 'path', 'publish_name', 'color_space',
+            'first_frame' and 'last_frame'
         """
 
-        path = item.properties["path"]
+        accepted = True
+        review_submission_app = self.parent.engine.apps.get("tk-multi-reviewsubmission")
+        if review_submission_app is None:
+            accepted = False
+            self.logger.warning(
+                "Review submission app is not available. skipping item: %s" %
+                (item.properties["publish_name"],)
+            )
+        if item.properties.get("color_space") is None:
+            accepted = False
+            self.logger.warning(
+                "'color_space' property is not defined on the item. "
+                "Item will be skipped: %s." %
+                (item.properties["publish_name"],)
+            )
+        if item.properties.get("first_frame") is None:
+            accepted = False
+            self.logger.warning(
+                "'first_frame' property is not defined on the item. "
+                "Item will be skipped: %s." %
+                (item.properties["publish_name"],)
+            )
+        if item.properties.get("last_frame") is None:
+            accepted = False
+            self.logger.warning(
+                "'last_frame' property is not defined on the item. "
+                "Item will be skipped: %s." %
+                (item.properties["publish_name"],)
+            )
 
-        # log the accepted file and display a button to reveal it in the fs
-        self.logger.info(
-            "Submit for review plugin accepted: %s" % (path,),
-            extra={
-                "action_show_folder": {
-                    "path": path
+
+        if accepted:
+            path = item.properties["path"]
+
+            # log the accepted file and display a button to reveal it in the fs
+            self.logger.info(
+                "Submit for review plugin accepted: %s" % (path,),
+                extra={
+                    "action_show_folder": {
+                        "path": path
+                    }
                 }
-            }
-        )
-
-        # return the accepted info
-        return {"accepted": True}
+            )
+        return {
+            "accepted": accepted,
+            "checked": True
+        }
 
     def validate(self, settings, item):
         """
@@ -263,56 +268,57 @@ class BasicSubmitForReviewPlugin(HookBaseClass):
         :param item: Item to process
         """
 
-        sg_publish = item.properties["sg_publish_data"]
+        render_path = item.properties.get("path")
+        if render_path is None:
+            raise Exception("'path' property cannot be found in item's properties. "
+                            "Review Submission failed.")
+
+        sg_publish_data = item.properties.get("sg_publish_data")
+        if sg_publish_data is None:
+            raise Exception("'sg_publish_data' was not found in the item's properties. "
+                            "Review Submission for '%s' failed." % render_path)
         sg_task = self.parent.context.task
         comment = item.description
         thumbnail_path = item.get_thumbnail_as_path()
         progress_cb = lambda *args, **kwargs: None
         review_submission_app = self.parent.engine.apps.get("tk-multi-reviewsubmission")
 
-        render_path = item.properties.get("path")
         render_template = item.properties.get("work_template")
+        if render_template is None:
+            raise Exception("'work_template' property is missing from item's properties. "
+                            "Review submission for '%s' failed." % render_path)
         publish_template = item.properties.get("publish_template")
+        if publish_template is None:
+            raise Exception("'publish_template' property not found on item. "
+                            "Review submission for '%' failed." % render_path)
+        if not render_template.validate(render_path):
+            raise Exception("'%s' did not match the render template. "
+                            "Review submission failed." % render_path)
+
         render_path_fields = render_template.get_fields(render_path)
         first_frame = item.properties.get("first_frame")
         last_frame = item.properties.get("last_frame")
-
-        if hasattr(review_submission_app, "render_and_submit_version"):
-            # this is a recent version of the review submission app that contains
-            # the new method that also accepts a colorspace argument.
-            colorspace = item.properties.get("color_space")
-            review_submission_app.render_and_submit_version(
+        colorspace = item.properties.get("color_space")
+        
+        if review_submission_app.render_and_submit_version(
                 publish_template,
                 render_path_fields,
                 first_frame,
                 last_frame,
-                [sg_publish],
+                [sg_publish_data],
                 sg_task,
                 comment,
                 thumbnail_path,
                 progress_cb,
                 colorspace
-            )
-        else:
-            # This is an older version of the app so fall back to the legacy
-            # method - this may mean the colorspace of the rendered movie is
-            # inconsistent/wrong!
-            review_submission_app.render_and_submit(
-                publish_template,
-                render_path_fields,
-                first_frame,
-                last_frame,
-                [sg_publish],
-                sg_task,
-                comment,
-                thumbnail_path,
-                progress_cb
-            )
+            ) is None:
+            raise Exception("Review submission failed. Could not render and "
+                            "submit the review associated sequence.")
 
     def finalize(self, settings, item):
         """
         Execute the finalization pass. This pass executes once all the publish
-        tasks have completed, and can for example be used to version up files.
+        tasks have completed.
 
         :param settings: Dictionary of Settings. The keys are strings, matching
             the keys returned in the settings property. The values are `Setting`
