efac37620146887d6a0f4320d547e5b7b85a8b64
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-10-24 08:07:29 -0400

    fixed test

--- tests/test_basic_publish_plugin.py
+++ tests/test_basic_publish_plugin.py
@@ -87,6 +87,7 @@ class TestBasicPublishPlugin(PublishApiTestBase):
         }
         item_dict = {"description": "description value", "thumbnail_path": "thumbnail_path value"}
         parent = self.PublishItem("blah", "blah", "blah")
+        parent.properties.sg_publish_data = {"type": "PublishedFile", "id": 32}
         publish_item = self.PublishItem("baz", "baz", "baz")
         publish_item._parent = parent
         publish_item._global_properties = self.PublishData.from_dict(global_props_dict)
@@ -107,6 +108,7 @@ class TestBasicPublishPlugin(PublishApiTestBase):
                 "thumbnail_path": item_dict["thumbnail_path"],
                 "published_file_type": global_props_dict["publish_type"],
                 "dependency_paths": global_props_dict["publish_dependencies"],
+                "dependency_ids": [32],
                 "sg_fields": global_props_dict["publish_fields"]
             }
             expected_kwargs.update(global_props_dict["publish_kwargs"])
