66b426af2fc17b972913cbb77d1d20a66ecafd9c
mathurf <francis.mathurin@autodesk.com>
2020-09-14 09:46:33 -0400

    Minor copyright updates

--- tests/fixtures/configInheritance/core/hooks/pick_environment.py
+++ tests/fixtures/configInheritance/core/hooks/pick_environment.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Autodesk, Inc.  All rights reserved.
+# Copyright 2020 Autodesk, Inc.  All rights reserved.
 #
 # Use of this software is subject to the terms of the Autodesk license agreement
 # provided at the time of installation or download, or which otherwise accompanies
