6141d5daa86d93d55c0232ffc022ff78e25d07e3
Jeff Beeland <jeff.beeland@autodesk.com>
2021-03-08 17:20:58 -0800

    Incorporates changes from client PR #99, plus one additional bit of cleanup for consistency.

--- python/tk_multi_publish2/api/manager.py
+++ python/tk_multi_publish2/api/manager.py
@@ -540,9 +540,9 @@ class PublishManager(object):
                 # current app instance.
                 for settings in context_settings:
                     if settings.get("app_instance") == self._bundle.instance_name:
-                        app_settings = settings
+                        app_settings = settings.get("settings")
             elif len(context_settings) == 1:
-                app_settings = context_settings[0]["settings"]
+                app_settings = context_settings[0].get("settings")
 
             if app_settings:
                 plugin_settings = app_settings[self.CONFIG_PLUGIN_DEFINITIONS]
