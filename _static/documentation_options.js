86acac4908290d91c417500a43218003d9bc59ed
Jean-Francois Boismenu <jean.francois.boismenu@autodesk.com>
2021-10-04 16:17:48 -0400

    Auto-updated documentation based on branch/tag v2.5.5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,11 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.6.0',
+    VERSION: 'v2.5.5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
 };
