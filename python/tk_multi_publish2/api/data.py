77da6442dc4b1c2a815de1897908898117c80b4f
andreassgm <106736675+andreassgm@users.noreply.github.com>
2022-06-02 11:06:54 +0200

    Python 3.10 Compatibility
    Changed the collections to collections.abc for compatibility with Python3.10

--- python/tk_multi_publish2/api/data.py
+++ python/tk_multi_publish2/api/data.py
@@ -8,7 +8,10 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import collections
+try:
+    import collections.abc as collections
+except ImportError:
+    import collections
 import sgtk
 import copy
 
