33f16d82cd895e579be59f4b308c8a10ddccbcaf
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2023-08-16 08:00:47 -0500

    Fix select items independently if visible is False

--- python/tk_multi_publish2/publish_tree_widget/publish_tree_widget.py
+++ python/tk_multi_publish2/publish_tree_widget/publish_tree_widget.py
@@ -497,26 +497,6 @@ class PublishTreeWidget(QtGui.QTreeWidget):
         root = self.invisibleRootItem()
         _check_r(root)
 
-    def set_check_state_for_invisible_siblings(self, plugin, state):
-        """
-        Set the check state for all invisible items,
-        otherwise there's no way to check back them.
-
-        :param plugin: Plugin for which tasks should be manipulated
-        :param state: checkstate to set.
-        """
-        logger.debug("Setting state %d for invisible sibling %s" % (state, plugin))
-
-        def _check_r(parent):
-            for child_index in range(parent.childCount()):
-                child = parent.child(child_index)
-                if isinstance(child, TreeNodeTask) and not child.task.visible:
-                    child.set_check_state(state)
-                _check_r(child)
-
-        root = self.invisibleRootItem()
-        _check_r(root)
-
     def dropEvent(self, event):
         """
         Something was dropped on this widget
