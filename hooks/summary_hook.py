ff2bff3e4f1430f5711acb34f0b9c3310bb6becf
Joseph Yu <joseph.yu@treehousevfx.studio>
2026-02-09 19:43:54 +0000

    Tidied

--- hooks/summary_hook.py
+++ hooks/summary_hook.py
@@ -19,43 +19,67 @@ HookBaseClass = sgtk.get_hook_baseclass()
 class SummaryHook(HookBaseClass):
     """Hook that defines the summary overlay content and display details."""
 
+    FALLBACK_SETTINGS = {
+        "no_item_error": {
+            "icon_path": ":/tk_multi_publish2/publish_failed.png",
+            # Hardcoding line break so the message displays on 2 lines.
+            # Usage of label's own word wrap displays the message below on 3 lines.
+            # NOTE: Can't manually break line when using <p></p>
+            "label_text": "Could not find any\nitems to publish.",
+            "info_text": "For more details, <b><u>click here</u></b>.",
+            "publish_again_text": "",
+        },
+        "success": {
+            "icon_path": ":/tk_multi_publish2/publish_complete.png",
+            "label_text": "Publish\nComplete",
+            "info_text": "For more details, <b><u>click here</u></b>.",
+            "publish_again_text": "To publish again, <b><u>click here</u></b>.",
+        },
+        "fail": {
+            "icon_path": ":/tk_multi_publish2/publish_failed.png",
+            "label_text": "Publish\nFailed!",
+            "info_text": "For more details, <b><u>click here</u></b>.",
+            "publish_again_text": "",
+        },
+        "loading": {
+            "icon_path": ":/tk_multi_publish2/overlay_loading.png",
+            "label_text": "Loading and processing",
+            "info_text": "Hold tight while we analyze your data",
+            "publish_again_text": "",
+        },
+    }
+    """Fallback settings to use for `.show_using_settings`, uses v2.10.7 values."""
+
     @property
     def settings(self) -> dict[str, Any]:
         """Return the settings that are available for this hook."""
         return self.parent.get_setting("summary").get("settings") or {}
 
+    def show_using_settings(self, key, summary_overlay) -> dict[str, Any]:
+        """Return UI values for the no items collected summary state."""
+        settings = self.settings.get(key, {})
+        fallback = self.FALLBACK_SETTINGS.get(key, {})
+        return summary_overlay.show_summary(
+            settings.get("icon_path", fallback["icon_path"]),
+            settings.get("label_text", fallback["label_text"]),
+            settings.get("info_text", fallback["info_text"]),
+            publish_again_text=settings.get(
+                "publish_again_text", fallback["publish_again_text"]
+            ),
+        )
+
     def no_items_error(self, summary_overlay) -> dict[str, Any]:
         """Return UI values for the no items collected summary state."""
-        return summary_overlay.show_summary(
-            ":/tk_multi_publish2/publish_failed.png",
-            # Hardcoding line break so the message displays on 2 lines.
-            # Usage of label's own word wrap displays the message below on 3 lines.
-            # NOTE: Can't manually break line when using <p></p>
-            "Could not find any\nitems to publish.",
-            "For more details, <b><u>click here</u></b>.",
-        )
+        return self.show_using_settings("no_item_error", summary_overlay)
 
     def success(self, summary_overlay) -> dict[str, Any]:
         """Return UI values for the publish success summary state."""
-        return summary_overlay.show_summary(
-            ":/tk_multi_publish2/publish_complete.png",
-            "Publish\nComplete",
-            "For more details, <b><u>click here</u></b>.",
-            publish_again_text="To publish again, <b><u>click here</u></b>.",
-        )
+        return self.show_using_settings("success", summary_overlay)
 
     def fail(self, summary_overlay) -> dict[str, Any]:
         """Return UI values for the publish fail summary state."""
-        return summary_overlay.show_summary(
-            ":/tk_multi_publish2/publish_failed.png",
-            "Publish\nFailed!",
-            "For more details, <b><u>click here</u></b>.",
-        )
+        return self.show_using_settings("fail", summary_overlay)
 
     def loading(self, summary_overlay) -> dict[str, Any]:
         """Return UI values for the loading summary state."""
-        return summary_overlay.show_summary(
-            ":/tk_multi_publish2/overlay_loading.png",
-            "Loading and processing",
-            "Hold tight while we analyze your data",
-        )
+        return self.show_using_settings("loading", summary_overlay)
--- tests/test_summary_overlay.py
+++ tests/test_summary_overlay.py
@@ -8,14 +8,16 @@
 # agreement to the ShotGrid Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Autodesk.
 
+from contextlib import contextmanager
 from unittest import mock
 
 from publish_api_test_base import PublishApiTestBase
 from tank_test.tank_test_base import setUpModule  # noqa
 
 
-class TestSummaryHook(PublishApiTestBase):
-    def check_values_match_v2_10_7(self, summary_method, expected_args):
+class TestSummaryOverlay(PublishApiTestBase):
+    @contextmanager
+    def check_values_match_v2_10_7(self, expected_args):
         """Test that calling method sets expected values."""
         from sgtk.platform.qt import QtGui
 
@@ -41,56 +43,53 @@ class TestSummaryHook(PublishApiTestBase):
                 expected_args,
             )
 
-            getattr(self.app.summary_hook, summary_method)(widget)
+            yield widget
 
+            assert widget.show.called
             assert widget.ui.icon.setPixmap.called
             for patched_method, expected_arg in patched_methods_expected_args:
                 patched_method.assert_called_with(expected_arg)
 
     def test_no_item_error(self):
-        self.check_values_match_v2_10_7(
-            "no_items_error",
-            [
-                ":/tk_multi_publish2/publish_failed.png",
-                "Could not find any\nitems to publish.",
-                "For more details, <b><u>click here</u></b>.",
-                "",
-                False,
-            ],
-        )
+        v2_10_7_values = [
+            ":/tk_multi_publish2/publish_failed.png",
+            "Could not find any\nitems to publish.",
+            "For more details, <b><u>click here</u></b>.",
+            "",
+            False,
+        ]
+        with self.check_values_match_v2_10_7(v2_10_7_values) as summary_overlay:
+            summary_overlay.show_no_items_error()
 
     def test_success(self):
-        self.check_values_match_v2_10_7(
-            "success",
-            [
-                ":/tk_multi_publish2/publish_complete.png",
-                "Publish\nComplete",
-                "For more details, <b><u>click here</u></b>.",
-                "To publish again, <b><u>click here</u></b>.",
-                True,
-            ],
-        )
+        v2_10_7_values = [
+            ":/tk_multi_publish2/publish_complete.png",
+            "Publish\nComplete",
+            "For more details, <b><u>click here</u></b>.",
+            "To publish again, <b><u>click here</u></b>.",
+            True,
+        ]
+        with self.check_values_match_v2_10_7(v2_10_7_values) as summary_overlay:
+            summary_overlay.show_success()
 
     def test_fail(self):
-        self.check_values_match_v2_10_7(
-            "fail",
-            [
-                ":/tk_multi_publish2/publish_failed.png",
-                "Publish\nFailed!",
-                "For more details, <b><u>click here</u></b>.",
-                "",
-                False,
-            ],
-        )
+        v2_10_7_values = [
+            ":/tk_multi_publish2/publish_failed.png",
+            "Publish\nFailed!",
+            "For more details, <b><u>click here</u></b>.",
+            "",
+            False,
+        ]
+        with self.check_values_match_v2_10_7(v2_10_7_values) as summary_overlay:
+            summary_overlay.show_fail()
 
     def test_loading(self):
-        self.check_values_match_v2_10_7(
-            "loading",
-            [
-                ":/tk_multi_publish2/overlay_loading.png",
-                "Loading and processing",
-                "Hold tight while we analyze your data",
-                "",
-                False,
-            ],
-        )
+        v2_10_7_values = [
+            ":/tk_multi_publish2/overlay_loading.png",
+            "Loading and processing",
+            "Hold tight while we analyze your data",
+            "",
+            False,
+        ]
+        with self.check_values_match_v2_10_7(v2_10_7_values) as summary_overlay:
+            summary_overlay.show_loading()
