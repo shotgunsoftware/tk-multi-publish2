2357d37c87f601b5069f58b4f8fa278c0016d9e5
chenm1 <ming.chen@autodesk.com>
2026-05-11 21:53:51 -0700

    fix code quality

--- python/tk_multi_publish2/utils/tga_image.py
+++ python/tk_multi_publish2/utils/tga_image.py
@@ -125,9 +125,7 @@ def tga_to_pixmap(path: str):
         else:
             fmt = QtGui.QImage.Format_Grayscale8
 
-        img = QtGui.QImage(
-            bytes(pixels), width, height, width * psize, fmt
-        )
+        img = QtGui.QImage(bytes(pixels), width, height, width * psize, fmt)
         if psize == 3:
             img = img.rgbSwapped()  # TGA stores BGR, Qt expects RGB
         if not (descriptor & 0x20):  # vertical origin: 0=bottom-left
