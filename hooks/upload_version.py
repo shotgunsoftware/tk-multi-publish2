8d5994b7d63303e8c6982437cd39d34d5e344325
Yungsiow Yang <yungsiow.yang@autodesk.com>
2026-08-12 17:08:48 -0400

    apply bg publish gating for upload version plugin

--- hooks/upload_version.py
+++ hooks/upload_version.py
@@ -241,81 +241,86 @@ class UploadVersionPlugin(HookBaseClass):
         :param item: Item to process
         """
 
-        publisher = self.parent
-        path = item.properties["path"]
-
-        # allow the publish name to be supplied via the item properties. this is
-        # useful for collectors that have access to templates and can determine
-        # publish information about the item that doesn't require further, fuzzy
-        # logic to be used here (the zero config way)
-        publish_name = item.properties.get("publish_name") or item.properties.get(
-            "publish_version_name"
-        )
-        if not publish_name:
-
-            self.logger.debug("Using path info hook to determine publish name.")
-
-            # use the path's filename as the publish name
-            path_components = publisher.util.get_file_path_components(path)
-            publish_name = path_components["filename"]
-
-        self.logger.debug("Publish name: %s" % (publish_name,))
-
-        self.logger.info("Creating Version...")
-        version_data = {
-            "project": item.context.project,
-            "code": publish_name,
-            "description": item.description,
-            "entity": self._get_version_entity(item),
-            "sg_task": item.context.task,
-        }
-
-        if "sg_publish_data" in item.properties:
-            publish_data = item.properties["sg_publish_data"]
-            version_data["published_files"] = [publish_data]
-
-        if settings["Link Local File"].value:
-            version_data["sg_path_to_movie"] = path
-
-        # log the version data for debugging
-        self.logger.debug(
-            "Populated Version data...",
-            extra={
-                "action_show_more_info": {
-                    "label": "Version Data",
-                    "tooltip": "Show the complete Version data dictionary",
-                    "text": "<pre>%s</pre>" % (pprint.pformat(version_data),),
-                }
-            },
-        )
-
-        # Create the version
-        version = publisher.shotgun.create("Version", version_data)
-        self.logger.info("Version created!")
-
-        # stash the version info in the item just in case
-        item.properties["sg_version_data"] = version
-
-        thumb = item.get_thumbnail_as_path()
-
-        if settings["Upload"].value:
-            self.logger.info("Uploading content...")
-
-            if sgtk.util.is_windows():
-                upload_path = str(path)
-            else:
-                upload_path = path
-
-            self.parent.shotgun.upload(
-                "Version", version["id"], upload_path, "sg_uploaded_movie"
+        if self._is_deferred_to_bg(item):
+            self.logger.info(
+                "Background publish enabled — deferring version upload to the background process."
             )
-        elif thumb:
-            # only upload thumb if we are not uploading the content. with
-            # uploaded content, the thumb is automatically extracted.
-            self.logger.info("Uploading thumbnail...")
-            self.parent.shotgun.upload_thumbnail("Version", version["id"], thumb)
+        else:
+            publisher = self.parent
+            path = item.properties["path"]
 
-        self.logger.info("Upload complete!")
+            # allow the publish name to be supplied via the item properties. this is
+            # useful for collectors that have access to templates and can determine
+            # publish information about the item that doesn't require further, fuzzy
+            # logic to be used here (the zero config way)
+            publish_name = item.properties.get("publish_name") or item.properties.get(
+                "publish_version_name"
+            )
+            if not publish_name:
+
+                self.logger.debug("Using path info hook to determine publish name.")
+
+                # use the path's filename as the publish name
+                path_components = publisher.util.get_file_path_components(path)
+                publish_name = path_components["filename"]
+
+            self.logger.debug("Publish name: %s" % (publish_name,))
+
+            self.logger.info("Creating Version...")
+            version_data = {
+                "project": item.context.project,
+                "code": publish_name,
+                "description": item.description,
+                "entity": self._get_version_entity(item),
+                "sg_task": item.context.task,
+            }
+
+            if "sg_publish_data" in item.properties:
+                publish_data = item.properties["sg_publish_data"]
+                version_data["published_files"] = [publish_data]
+
+            if settings["Link Local File"].value:
+                version_data["sg_path_to_movie"] = path
+
+            # log the version data for debugging
+            self.logger.debug(
+                "Populated Version data...",
+                extra={
+                    "action_show_more_info": {
+                        "label": "Version Data",
+                        "tooltip": "Show the complete Version data dictionary",
+                        "text": "<pre>%s</pre>" % (pprint.pformat(version_data),),
+                    }
+                },
+            )
+
+            # Create the version
+            version = publisher.shotgun.create("Version", version_data)
+            self.logger.info("Version created!")
+
+            # stash the version info in the item just in case
+            item.properties["sg_version_data"] = version
+
+            thumb = item.get_thumbnail_as_path()
+
+            if settings["Upload"].value:
+                self.logger.info("Uploading content...")
+
+                if sgtk.util.is_windows():
+                    upload_path = str(path)
+                else:
+                    upload_path = path
+
+                self.parent.shotgun.upload(
+                    "Version", version["id"], upload_path, "sg_uploaded_movie"
+                )
+            elif thumb:
+                # only upload thumb if we are not uploading the content. with
+                # uploaded content, the thumb is automatically extracted.
+                self.logger.info("Uploading thumbnail...")
+                self.parent.shotgun.upload_thumbnail("Version", version["id"], thumb)
+
+            self.logger.info("Upload complete!")
 
     def finalize(self, settings, item):
         """
@@ -328,19 +333,24 @@ class UploadVersionPlugin(HookBaseClass):
         :param item: Item to process
         """
 
-        path = item.properties["path"]
-        version = item.properties["sg_version_data"]
+        if self._is_deferred_to_bg(item):
+            self.logger.info(
+                "Background publish enabled — deferring version upload finalize to the background process."
+            )
+        else:
+            path = item.properties["path"]
+            version = item.properties["sg_version_data"]
 
-        self.logger.info(
-            "Version uploaded for file: %s" % (path,),
-            extra={
-                "action_show_in_shotgun": {
-                    "label": "Show Version",
-                    "tooltip": "Reveal the version in Flow Production Tracking.",
-                    "entity": version,
-                }
-            },
-        )
+            self.logger.info(
+                "Version uploaded for file: %s" % (path,),
+                extra={
+                    "action_show_in_shotgun": {
+                        "label": "Show Version",
+                        "tooltip": "Reveal the version in Flow Production Tracking.",
+                        "entity": version,
+                    }
+                },
+            )
 
     def _get_version_entity(self, item):
         """
@@ -399,3 +409,14 @@ class UploadVersionPlugin(HookBaseClass):
             publish_type = "Folder"
 
         return publish_type
+
+    def _is_deferred_to_bg(self, item) -> bool:
+        """Return True if publish item or any ancestor of it has bg publish enabled
+        but we are not currently in a bg process.
+        """
+        root = item
+        while not root.is_root:
+            root = root.parent
+        return bool(root.properties.get("bg_processing")) and not bool(
+            root.properties.get("in_bg_process")
+        )
